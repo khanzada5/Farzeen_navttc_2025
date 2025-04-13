cde73d2a2b370f24e763e399b0eceb28371b9606
Farzeen Khanzada <fkkhanzada29@gmail.com>
2025-04-14 04:18:23 +0500

    Add files via upload

---
+++ Task_18/script.js
@@ -0,0 +1,13 @@
+let name = "Farzeen Khanzada"
+let skill = "web developer"
+
+document.body.innerHTML =  "<h1>" + name + "</h1>"
+// document.body.innerHTML =  skill
+    
+// console.log (name)
+// console.log (skill)
+
+// console.log ( name , "is a Professional" , skill  )
+
+// alert(name)
+// confirm(skill)
